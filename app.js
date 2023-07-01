942ed279c0ba4fe234c1363cd8e3ba1748274034
Byomkesh Mahato <byomkeshmahato03@gmail.com>
2023-07-02 01:09:49 +0530

    geting input values from the from

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // Global array to store invoices
 let invoices = [];
+
+// Function to create a new invoice
+function createInvoice() {
+    // Get the input values from the form
+  const lineItems = document.getElementById('line-items').value;
+  const notes = document.getElementById('notes').value;
+  const dueDate = document.getElementById('due-date').value;
+
+};
