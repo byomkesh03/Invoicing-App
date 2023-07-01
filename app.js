db58615e2048dfd2794edd13c05ddd5498c85ecd
Byomkesh Mahato <byomkeshmahato03@gmail.com>
2023-07-02 02:04:51 +0530

    displaying  invoices and appending html elements

--- app.js
+++ app.js
@@ -27,3 +27,46 @@ invoices.push(invoice);
   // Call a function to display the newly created invoice
   displayInvoices();
 }
+
+// Function to display invoices
+function displayInvoices() {
+    // Get the 'view-invoices' section element
+    const viewInvoicesSection = document.getElementById('view-invoices');
+  
+    // Clear the existing content
+    viewInvoicesSection.innerHTML = '';
+  
+    // Loop through the invoices array
+    invoices.forEach((invoice, index) => {
+      // Create HTML elements to display each invoice
+      const invoiceContainer = document.createElement('div');
+      invoiceContainer.classList.add('invoice');
+
+      const invoiceLineItems = document.createElement('p');
+    invoiceLineItems.textContent = `Line Items: ${invoice.lineItems}`;
+
+    const invoiceNotes = document.createElement('p');
+    invoiceNotes.textContent = `Notes: ${invoice.notes}`;
+
+    const invoiceDueDate = document.createElement('p');
+    invoiceDueDate.textContent = `Due Date: ${invoice.dueDate}`;
+
+    const invoiceStatus = document.createElement('p');
+    invoiceStatus.textContent = `Status: ${invoice.status}`;
+
+    const invoiceDeleteBtn = document.createElement('button');
+    invoiceDeleteBtn.textContent = 'Delete';
+    invoiceDeleteBtn.addEventListener('click', () => {
+      deleteInvoice(index);
+    });
+
+     // Append the HTML elements to the 'view-invoices' section
+     invoiceContainer.appendChild(invoiceLineItems);
+     invoiceContainer.appendChild(invoiceNotes);
+     invoiceContainer.appendChild(invoiceDueDate);
+     invoiceContainer.appendChild(invoiceStatus);
+     invoiceContainer.appendChild(invoiceDeleteBtn);
+ 
+     viewInvoicesSection.appendChild(invoiceContainer);
+   });
+ }
